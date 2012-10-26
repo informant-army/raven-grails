be5f72e41870bc79a6c2a990dd8d0edcb17ed0ca
Alan Fachini <alan.fachini@informant.com.br>
2012-10-26 16:41:46 -0200

    SentryClient refactoring.

--- src/groovy/grails/plugins/sentry/SentryClient.groovy
+++ src/groovy/grails/plugins/sentry/SentryClient.groovy
@@ -1,9 +1,9 @@
 package grails.plugins.sentry
 
 import net.kencochrane.sentry.RavenConfig
-import net.kencochrane.sentry.RavenUtils
-
 import java.util.Date
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletRequestWrapper
 import org.apache.commons.lang.time.DateFormatUtils
 import static org.apache.commons.codec.binary.Base64.encodeBase64String
 
@@ -20,29 +20,28 @@ class SentryClient {
         this.connection = new SentryConnection(config)
     }
 
-    public void logMessage(String log, String loggerClass, int logLevel, String culprit, Throwable exception) {
-        long timestamp = timestampLong()
-        String message = buildMessage(log, timestampString(timestamp), loggerClass, logLevel, culprit, exception)
-        send(message, timestamp)
+    def logInfo(String message) {
+        logMessage(message, "root", 'info')
     }
 
-    def captureMessage(String message, String loggerClass, int logLevel, String culprit) {
+    def logMessage(String message, String loggerClass, String logLevel) {
         long timestamp = timestampLong()
-        String body = buildMessage(message, timestampString(timestamp), loggerClass, logLevel, culprit, null)
+        String body = buildMessage(message, null, loggerClass, logLevel, null, timestampString(timestamp))
         send(body, timestamp)
     }
 
-    def captureMessage(String message) {
-        captureMessage(message, "root", 50, null)
+    def logException(Throwable exception) {
+        logException(exception, "root", "error")
     }
 
-    def captureException(String message, long timestamp, String loggerClass, int logLevel, String culprit, Throwable exception) {
-        String body = buildMessage(message, RavenUtils.getTimestampString(timestamp), loggerClass, logLevel, culprit, exception);
+    def logException(Throwable exception, String loggerClass, String logLevel)  {
+        logException(exception, loggerClass, logLevel, null)
+    }
+
+    def logException(Throwable exception, String loggerClass, String logLevel, HttpServletRequest request) {
+        long timestamp = timestampLong()
+        String body = buildMessage(exception.getMessage(), exception, loggerClass, logLevel, request, timestampString(timestamp))
         send(body, timestamp)
-    }
-
-    def captureException(Throwable exception) {
-        captureException(exception.getMessage(), RavenUtils.getTimestampLong(), "root", 50, null, exception)
     }
 
     private void send(String message, long timestamp) {
@@ -53,9 +52,9 @@ class SentryClient {
         }
     }
 
-    private String buildMessage(String message, String timestamp, String loggerClass, int logLevel, String culprit, Throwable exception) {
+    private String buildMessage(String message, Throwable exception, String loggerClass, String logLevel, HttpServletRequest request, String timestamp) {
         SentryJSON json = new SentryJSON(this.config)
-        String jsonMessage = json.build(message, timestamp, loggerClass, logLevel, culprit, exception)
+        String jsonMessage = json.build(message, exception, loggerClass, logLevel, request, timestamp)
 
         return buildMessageBody(jsonMessage)
     }
