e25ce58d98814166db697cb8c2a2b0fcd8b8c886
Alan Fachini <alan.fachini@informant.com.br>
2012-11-09 16:32:55 -0200

    Set log level to error.

--- src/groovy/grails/plugins/sentry/SentryAppender.groovy
+++ src/groovy/grails/plugins/sentry/SentryAppender.groovy
@@ -1,6 +1,7 @@
 package grails.plugins.sentry
 
 import org.apache.log4j.AppenderSkeleton
+import org.apache.log4j.Level
 import org.apache.log4j.spi.LoggingEvent
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
@@ -11,6 +12,7 @@ class SentryAppender extends AppenderSkeleton {
     public SentryAppender() {
         super()
         sentryClient = new SentryClient(getDSN())
+        setThreshold(Level.ERROR)
     }
 
     void append(LoggingEvent event) {
