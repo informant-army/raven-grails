f1fd3c7913e5911a08c5587c0ba63bed9f764fe1
Alan Fachini <alfakini@gmail.com>
2012-12-12 17:24:51 -0200

    Update version to 0.5.

--- RavenGrailsPlugin.groovy
+++ RavenGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import grails.plugins.raven.RavenClient
 import grails.plugins.raven.Configuration
 
 class RavenGrailsPlugin {
-    def version = "0.4.4"
+    def version = "0.5"
     def clientVersion = "Raven-grails $version"
     def grailsVersion = "1.3.9 > *"
     def dependsOn = [:]
