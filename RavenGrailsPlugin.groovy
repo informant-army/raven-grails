9be4e9a8d2fcfa7406c0aee7d56308eadded43d6
Alan Fachini <alfakini@gmail.com>
2014-01-03 03:10:35 -0200

    Bump version to 0.5.4.

--- RavenGrailsPlugin.groovy
+++ RavenGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import grails.plugins.raven.RavenClient
 import grails.plugins.raven.Configuration
 
 class RavenGrailsPlugin {
-    def version = "0.5.4-SNAPSHOT"
+    def version = "0.5.4"
     def clientVersion = "Raven-grails $version"
     def grailsVersion = "1.3.9 > *"
     def dependsOn = [:]
