5ad729b908a784f84943967b263dcdf7e1569b07
Jakub Glapa <jakub.glapa@pmedianetwork.com>
2014-03-10 13:50:41 +0100

    support for additional attributes in User object

--- test/unit/grails/plugins/raven/interfaces/UserTests.groovy
+++ test/unit/grails/plugins/raven/interfaces/UserTests.groovy
@@ -1,5 +1,6 @@
 package grails.plugins.raven.interfaces
 
+import grails.converters.JSON
 import grails.plugins.raven.interfaces.User
 import grails.test.*
 import org.codehaus.groovy.grails.web.json.JSONObject
@@ -7,6 +8,7 @@ import org.codehaus.groovy.grails.web.json.JSONObject
 class UserTests extends GroovyTestCase {
 
     Map userTest = [id: 123, is_authenticated: true, username: 'username', email: 'user@email.com']
+    Map userTestWithAdditionalAttributes = [id: 123, is_authenticated: true, username: 'username', email: 'user@email.com', client: 'clientOne', department:"depOne"]
 
     protected void setUp() {
         super.setUp()
@@ -30,5 +32,35 @@ class UserTests extends GroovyTestCase {
 
         def result = user.toJSON().toString()
         assertEquals '{"id":"123","username":"username","email":"user@email.com","is_authenticated":true}', result
+    }
+
+    def void testUserDataWithAdditionalAttributes(){
+
+        User user = new User(userTestWithAdditionalAttributes.is_authenticated, userTestWithAdditionalAttributes)
+
+        assertEquals userTestWithAdditionalAttributes.id.toString(), user.id
+        assertEquals userTestWithAdditionalAttributes.username, user.username
+        assertEquals userTestWithAdditionalAttributes.is_authenticated, user.is_authenticated
+        assertEquals userTestWithAdditionalAttributes.email, user.email
+
+        assertEquals userTestWithAdditionalAttributes.client, user.getData().client
+        assertEquals userTestWithAdditionalAttributes.department, user.getData().department
+    }
+
+    def void testUserDataWithAdditionalAttributesToJSON(){
+
+        User user = new User(userTestWithAdditionalAttributes.is_authenticated, userTestWithAdditionalAttributes)
+
+        def result = user.toJSON().toString()
+        def resultAsJSON = JSON.parse(result)
+
+        assertEquals resultAsJSON.id.toString(), user.id
+        assertEquals resultAsJSON.username, user.username
+        assertEquals resultAsJSON.is_authenticated, user.is_authenticated
+        assertEquals resultAsJSON.email, user.email
+
+        assertEquals resultAsJSON.client, user.getData().client
+        assertEquals resultAsJSON.department, user.getData().department
+
     }
 }
